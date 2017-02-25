d64146d905b9a7b737adaa7e99a88adf346c97eb
NickvanDyke <nickjvandyke15@gmail.com>
2017-02-24 20:13:31 -0600

    Change button appearance and locations

--- core/src/screens/MenuScreen.java
+++ core/src/screens/MenuScreen.java
@@ -2,8 +2,8 @@ package screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -15,6 +15,8 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.powerpong.game.PowerPong;
 
+import java.awt.*;
+
 public class MenuScreen implements Screen {
     public enum Mode {
         CLASSIC, AIBATTLE, WALL
@@ -22,7 +24,7 @@ public class MenuScreen implements Screen {
     public static Mode mode;
 	private Stage stage;
 	private Table table;
-	private Table left, right;
+	private Table modes, difficulties;
 	private Skin skin;
 	private PowerPong game;
 	private AIBattle ai;
@@ -40,51 +42,58 @@ public class MenuScreen implements Screen {
 		skin.add("background", new Texture("MenuBackground.png"));
 
 		// Generate a font and add it to the skin under the name "Xcelsion"
-		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Xcelsion.ttf"));
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/ARCADECLASSIC.ttf"));
 		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-		parameter.size = 70;
-		skin.add("Xcelsion", generator.generateFont(parameter));
+		parameter.size = 175;
+		skin.add("font", generator.generateFont(parameter));
 
 		//get the TextButtonStyle defined in the JSON under the name "default" and then modify it
 		TextButton.TextButtonStyle textButtonStyle = skin.get(TextButton.TextButtonStyle.class);
-		textButtonStyle.font = skin.getFont("Xcelsion");
-		textButtonStyle.checked = textButtonStyle.down;
+		textButtonStyle.font = skin.getFont("font");
+        textButtonStyle.checked = null;
+		textButtonStyle.up = null;
+		textButtonStyle.down = null;
+		textButtonStyle.over = null;
+		textButtonStyle.fontColor = Color.WHITE;
+		textButtonStyle.overFontColor = Color.GRAY;
 
 		// Create a table that fills the screen. Everything else will go inside this table.
 		table = new Table();
 		table.setSkin(skin); //set the table's skin. This means that all widgets within this table will use the skin's definitions by default
 		table.setBackground("background");
 		table.setFillParent(true);
-		table.align(Align.left);
 		stage.addActor(table);
 
 
-        //stuff for the left VerticalGroup; gamemodes, options, etc
-        left = new Table();
+        //stuff for the modes VerticalGroup; gamemodes, options, etc
+        modes = new Table();
         // Create a button with the "default" TextButtonStyle of skin. A 3rd parameter can be used to specify a name other than "default".
-        final TextButton button1P = new TextButton("One\nPlayer", skin);
-        left.add(button1P).fillX();
-        left.row();
-        final TextButton button2P = new TextButton("Two\nPlayer", skin);
-        left.add(button2P).fillX();
-        left.row();
-        final TextButton buttonWall = new TextButton("Wall\nMode", skin);
-        left.add(buttonWall).fillX();
-        left.row();
-        final TextButton buttonAIBattle = new TextButton("AI\nBattle", skin);
-        left.add(buttonAIBattle).fillX();
-        left.row();
+        final TextButton button1P = new TextButton("One Player", skin);
+        button1P.setHeight(175);
+        button1P.setWidth(button1P.getPrefWidth() + 50);
+        modes.add(button1P).width(button1P.getWidth()).height(button1P.getHeight());
+        modes.row();
+        final TextButton button2P = new TextButton("Two Player", skin);
+        modes.add(button2P).fillX().height(button1P.getHeight());
+        modes.row();
+        final TextButton buttonWall = new TextButton("Wall Mode", skin);
+        modes.add(buttonWall).fillX().height(button1P.getHeight());
+        modes.row();
+        final TextButton buttonAIBattle = new TextButton("AI Battle", skin);
+        modes.add(buttonAIBattle).fillX().height(button1P.getHeight());
+        modes.row();
         final TextButton buttonOptions = new TextButton("Options", skin);
-        left.add(buttonOptions).fillX().prefHeight(button1P.getPrefHeight());
+        modes.add(buttonOptions).fillX().height(button1P.getHeight());
         // Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
         // Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
         // ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
         // revert the checked state.
         button1P.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
-                right.setVisible(button1P.isChecked());
+                difficulties.setVisible(button1P.isChecked());
                 if (buttonAIBattle.isChecked())
                     buttonAIBattle.setChecked(false);
+                modes.setVisible(false);
                 mode = Mode.CLASSIC;
             }
         });
@@ -102,28 +111,31 @@ public class MenuScreen implements Screen {
         });
         buttonAIBattle.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
-                right.setVisible(buttonAIBattle.isChecked());
+                difficulties.setVisible(buttonAIBattle.isChecked());
                 if (button1P.isChecked())
                     button1P.setChecked(false);
+                modes.setVisible(false);
                 mode = Mode.AIBATTLE;
             }
         });
 
-        //right stuff; ai difficulties
-        right = new Table();
-        right.setVisible(false);
+        //difficulties stuff; ai difficulties
+        difficulties = new Table();
+        difficulties.setVisible(false);
         final TextButton buttonEasy = new TextButton("Easy", skin);
-        right.add(buttonEasy).fillX().prefHeight(button1P.getPrefHeight());
-        right.row();
+        difficulties.add(buttonEasy).width(button1P.getWidth()).height(button1P.getHeight());
+        difficulties.row();
         final TextButton buttonMedium = new TextButton("Medium", skin);
-        right.add(buttonMedium).fillX().prefHeight(button1P.getPrefHeight());
-        right.row();
+        difficulties.add(buttonMedium).fillX().height(button1P.getHeight());
+        difficulties.row();
         final TextButton buttonHard = new TextButton("Hard", skin);
-        right.add(buttonHard).fillX().prefHeight(button1P.getPrefHeight());
-        right.row();
+        difficulties.add(buttonHard).fillX().height(button1P.getHeight());
+        difficulties.row();
         final TextButton buttonSkynet = new TextButton("Skynet", skin);
-        right.add(buttonSkynet).fillX().prefHeight(button1P.getPrefHeight());
-
+        difficulties.add(buttonSkynet).fillX().height(button1P.getHeight());
+        difficulties.row();
+        final TextButton buttonBack = new TextButton("Back", skin);
+        difficulties.add(buttonBack).fillX().height(button1P.getHeight());
         buttonEasy.addListener(new ChangeListener() {
             public void changed (ChangeEvent event, Actor actor) {
                 startPlay(PlayScreen.AI.EASY);
@@ -144,9 +156,22 @@ public class MenuScreen implements Screen {
                 startPlay(PlayScreen.AI.SKYNET);
             }
         });
+        buttonBack.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                difficulties.setVisible(false);
+                modes.setVisible(true);
+                button1P.setChecked(false);
+                buttonAIBattle.setChecked(false);
+            }
+        });
 
-        table.add(left).top();
-        table.add(right).top();
+        Stack menu = new Stack();
+        menu.add(modes);
+        menu.add(difficulties);
+        stage.addActor(menu);
+        menu.setX(PowerPong.NATIVE_WIDTH / 2 - menu.getWidth() / 2);
+        menu.setY(800);
+        //table.add(menu);
 
         ai = new AIBattle(game, PlayScreen.AI.SKYNET);
 	}
