b77588a434d8a9c137d020ae7d4be480ec2ee875
NickvanDyke <nickjvandyke15@gmail.com>
2017-03-05 22:58:05 -0600

    Formatting

--- desktop/src/com/pongplus/game/desktop/DesktopLauncher.java
+++ desktop/src/com/pongplus/game/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.pongplus.game.PongPlus;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.height = PongPlus.VIRTUAL_HEIGHT / 2;
 		config.width = PongPlus.VIRTUAL_WIDTH / 2;
